72de78e17fa919f3ed6c61db35f23fa97454ca02
João Ricardo <ricardo.forttunato@gmail.com>
2023-08-01 17:07:24 -0300

    Changing typo differences

--- day2tipCalculator.py
+++ day2tipCalculator.py
@@ -1,8 +1,8 @@
 # # Build a tip calculator.
 print("Welcome to the tip calculator")
-value_bill = float(input("what's the total bill? 10, 12 or 15? "))
-value_tip = float(input("What percentage tip would you like to give? "))
-number_of_pay = float(input("How many people to split the bill? "))
+value_bill = float(input("what's the total bill? $"))
+value_tip = float(input("What percentage tip would you like to give? 10, 12 or 15? "))
+number_of_pay = int(input("How many people to split the bill? "))
 
 indivdual_bill = (value_bill / number_of_pay)
 total = indivdual_bill + ((value_bill * (value_tip / 100)) /number_of_pay)
