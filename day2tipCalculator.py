1cb0352817cff1a1f9be2b9d5201f4545d86b89f
João Ricardo <ricardo.forttunato@gmail.com>
2023-08-01 16:53:13 -0300

    Add some code to file day2TipCalculator.py

--- day2tipCalculator.py
+++ day2tipCalculator.py
@@ -1,1 +1,10 @@
-# Build a tip calculator.
+# # Build a tip calculator.
+print("Welcome to the tip calculator")
+value_bill = float(input("what's the total bill? 10, 12 or 15? "))
+value_tip = float(input("What percentage tip would you like to give? "))
+number_of_pay = float(input("How many people to split the bill? "))
+
+indivdual_bill = (value_bill / number_of_pay)
+total = indivdual_bill + ((value_bill * (value_tip / 100)) /number_of_pay)
+
+print("Each person should pay: $ %.2f" %(total))
