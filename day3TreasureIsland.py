3e8fe85abac403cb6c676bf67e8a18656f57b7e2
João Ricardo <ricardo.forttunato@gmail.com>
2023-08-03 14:56:31 -0300

    Add Love Calculator code in the source file.

--- day3TreasureIsland.py
+++ day3TreasureIsland.py
@@ -1,31 +1,31 @@
-height = int(input("Enter your height in cm? "))
-bill = 0
+# height = int(input("Enter your height in cm? "))
+# bill = 0
 
-if height > 120:
-    print("You can ride the rollercoaster!")
-    age = int(input("What is your age? "))
-    if age < 12:
-        bill = 5
-        print("Please pay $5.")
-    elif age <= 18:
-        bill = 7
-        print("Please pay $7.")
-    elif age >= 45 and age <= 55:
-        bill = 0
-        print("Everything is going to be ok. Have a free ride on us!")
-    else:
-        bill = 12
-        print("Please pay $12.")
+# if height > 120:
+#     print("You can ride the rollercoaster!")
+#     age = int(input("What is your age? "))
+#     if age < 12:
+#         bill = 5
+#         print("Please pay $5.")
+#     elif age <= 18:
+#         bill = 7
+#         print("Please pay $7.")
+#     elif age >= 45 and age <= 55:
+#         bill = 0
+#         print("Everything is going to be ok. Have a free ride on us!")
+#     else:
+#         bill = 12
+#         print("Please pay $12.")
 
-    wants_photo = input("Do you want a photo taken? Y or N. ")
-    if wants_photo == "Y" or "y":
-        bill += 3
-    else:
-        bill = bill
-    print(f"Your final bill is ${bill}.")
+#     wants_photo = input("Do you want a photo taken? Y or N. ")
+#     if wants_photo == "Y" or "y":
+#         bill += 3
+#     else:
+#         bill = bill
+#     print(f"Your final bill is ${bill}.")
     
-else:
-    print("Sorry, you have to grow taller before you can ride.")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
 
 
 # year = int(input("input year: "))
@@ -60,3 +60,23 @@ else:
 #     bill += 1
 
 # print(f"Your final bill is: ${bill}")
+
+# Love Calculator Exercise.
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+name = name1.lower() + name2.lower()
+
+true = name.count("t") + name.count("r") + name.count("u") + name.count("e")
+love = name.count("l") + name.count("o") + name.count("v") + name.count("e")
+
+resultString = str(true) + str(love)
+
+loveScore = int(resultString)
+
+if loveScore < 10 or loveScore > 90:
+    print(f"Your love score is {loveScore}, you go together like coke and mentos.")
+elif loveScore >= 40 and loveScore <= 50:
+    print(f"Your love score is {loveScore}, you are alright together.")
+else:
+    print(f"Your score is {loveScore}.")
